68aa55343bb152330df95a7597b0692b478abffa
Nicholas Rose <47899789+DualOrb@users.noreply.github.com>
2024-03-04 08:54:45 -0500

    User accounts and basic nav bar

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+    header("Location: auth/login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <title>User Dashboard</title>
+</head>
+<body style="padding: 0px;">
+<?php
+require_once("partials/navbar.php")
+?>
+<!--<div class="container">-->
+<!---->
+<!--    <h1>Welcome to Dashboard</h1>-->
+<!--    <a href="auth/logout.php" class="btn btn-warning">Logout</a>-->
+<!--</div>-->
+</body>
+</html>
